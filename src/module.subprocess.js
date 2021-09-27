83d2830305d7f6280131b15c2e6e8e50eaab791b
Simon Petrac <pfoerdie@gmail.com>
2021-09-27 17:13:16 +0200

    LongRunningProcess

--- src/module.subprocess.js
+++ src/module.subprocess.js
@@ -21,18 +21,18 @@ exports.ExecutionProcess = function (exe, cwd = process.cwd(), verbose = false) 
     async function processor(...args) {
         const subprocess = child_process.spawn(exe, util.flattenArgs(args), {cwd});
         if (verbose) process.stdout.write('$ ' + cwd + '> ' + subprocess.spawnargs.join(' ') + '\n');
-        const _out = [], _err = [];
+        const stdout = [], stderr = [];
         subprocess.stdout.on('data', (data) => {
-            _out.push(data);
+            stdout.push(data);
             if (verbose) process.stdout.write(data);
         });
         subprocess.stderr.on('data', (data) => {
-            _err.push(data);
+            stderr.push(data);
             if (verbose) process.stderr.write(data);
         });
         const exitCode = await new Promise((resolve) => subprocess.on('close', resolve));
-        if (exitCode !== 0) throw new Error(_err.join(''));
-        return _out.join('') || _err.join('');
+        if (exitCode !== 0) throw new Error(stderr.join(''));
+        return stdout.join('') || stderr.join('');
     } // processor
 
     return processor;
@@ -42,7 +42,7 @@ exports.ExecutionProcess = function (exe, cwd = process.cwd(), verbose = false) 
  * @param {string} exe
  * @param {string} [cwd=process.cwd()]
  * @param {boolean} [verbose=false]
- * @returns {function(...any): EventEmitter}
+ * @returns {function(...any): module:events.EventEmitter}
  */
 exports.LongRunningProcess = function (exe, cwd = process.cwd(), verbose = false) {
     util.assert(util.isExecutable(exe), 'createProcessor : expected exe to be an executable string', TypeError);
@@ -52,7 +52,30 @@ exports.LongRunningProcess = function (exe, cwd = process.cwd(), verbose = false
     function processor(...args) {
         const subprocess = child_process.spawn(exe, util.flattenArgs(args), {cwd});
         if (verbose) process.stdout.write('$ ' + cwd + '> ' + subprocess.spawnargs.join(' ') + '\n');
-        // TODO
+        const emitter = new EventEmitter();
+        let lastRow   = '';
+        subprocess.stdout.on('data', (data) => {
+            emitter.emit('stdout', data);
+            const rows = data.toString().split(/\r?\n/g);
+            rows[0]    = lastRow + rows[0];
+            lastRow    = rows.pop();
+            for (let row of rows) {
+                emitter.emit('data', row);
+            }
+            if (verbose) process.stdout.write(data);
+        });
+        subprocess.stderr.on('data', (data) => {
+            emitter.emit('stderr', data);
+            if (verbose) process.stderr.write(data);
+        });
+        subprocess.on('close', (exitCode) => {
+            if (lastRow) emitter.emit('data', lastRow);
+            emitter.emit('end', exitCode);
+        });
+        emitter.on('close', (signal) => {
+            subprocess.kill(signal);
+        });
+        return emitter;
     } // processor
 
     return processor;
--- test/subprocess.test.js
+++ test/subprocess.test.js
@@ -1,7 +1,7 @@
 const
-    {describe, test}   = require('mocha'),
-    expect             = require('expect'),
-    {ExecutionProcess} = require('../src/module.subprocess.js');
+    {describe, test}                       = require('mocha'),
+    expect                                 = require('expect'),
+    {ExecutionProcess, LongRunningProcess} = require('../src/module.subprocess.js');
 
 describe('module.subprocess', function () {
 
@@ -10,6 +10,19 @@ describe('module.subprocess', function () {
         test('ping', async function () {
             const ping = ExecutionProcess('ping');
             console.log(await ping({n: 1}, 'google.com'));
+        });
+
+    });
+
+    describe('LongRunningProcess', function () {
+
+        test('ping', async function () {
+            const ping = LongRunningProcess('ping');
+            await new Promise((resolve) => {
+                ping({n: 1}, 'google.com')
+                    .on('data', console.log)
+                    .on('end', resolve);
+            });
         });
 
     });
